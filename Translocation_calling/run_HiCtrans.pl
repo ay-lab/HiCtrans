819eee3c4fe19820a01c6d7e126b8041c4da7b58
ay-lab <ferhatay@lji.org>
2018-04-02 19:46:21 -0700

    Update run_HiCtrans.pl

--- Translocation_calling/run_HiCtrans.pl
+++ Translocation_calling/run_HiCtrans.pl
@@ -6,6 +6,10 @@ $chrB = @input[3];
 $folder = @input[4];
 $count_filter = 10; #Filter out intergenic-interactions below this contact count; Can be changed based on sample. 
 
+## 04/02/2018 ##
+$ratio_filter = 2;  #This is the box.entropy/random.entropy ratio. This is will control the noise effect
+################
+
 $stamp = `date|awk '{print \$3_\$2_\$6}'`; 
 $job   = $$;
 chomp ($stamp,$job);
@@ -15,11 +19,21 @@ if ($chrA ne "all" && $chrB ne "all"){
 	print out "Rscript ../scripts/translocationFind.r $chrA-$chrB/$chrA-$chrB.count.matrix $chrA-$chrB\n";
 	print out "Rscript ../scripts/maxCountFilter.r $chrA-$chrB/$chrA-$chrB.fragsfile $chrA-$chrB/$chrA-$chrB.intersfile $chrA $chrB $chrA-$chrB\n";
 	print out "perl ../scripts/mapBack.pl $chrA $chrB $chrA-$chrB/$chrA-$chrB.fragsfile $chrA-$chrB.tmp.result > $chrA-$chrB.Translocation.result\n";
+
+	## 04/02/2018 ##
+	print out "Rscript ../scripts/noiseReduction.r -f $chrA-$chrB/$chrA-$chrB.intersfile -t $chrA-$chrB.Translocation.result -s 10 -c $count_filter -n $ratio_filter -o $chrA-$chrB.Translocation.EntropyFiltered.result\n"; 
+	################
+	
 	print out "mkdir $folder\n";
 	print out "mv $input $folder/\n";
 	print out "mv $chrA-$chrB $folder/\n";
 	print out "mv $chrA-$chrB.tmp.result $folder/\n";
 	print out "mv $chrA-$chrB.Translocation.result $folder/";
+	
+	## 04/02/2018 ##
+	print out "mv $chrA-$chrB.Translocation.EntropyFiltered.result $folder/";
+	################
+	
 	close out;
 }
 elsif ($chrA eq "all" && $chrB eq "all"){
@@ -44,17 +58,35 @@ elsif ($chrA eq "all" && $chrB eq "all"){
         		print out "Rscript ../scripts/translocationFind.r $chr[$i]-$chr[$j]/$chr[$i]-$chr[$j].count.matrix $chr[$i]-$chr[$j]\n";
         		print out "Rscript ../scripts/maxCountFilter.r $chr[$i]-$chr[$j]/$chr[$i]-$chr[$j].fragsfile $chr[$i]-$chr[$j]/$chr[$i]-$chr[$j].intersfile $chr[$i] $chr[$j] $chr[$i]-$chr[$j]\n";
         		print out "perl ../scripts/mapBack.pl $chr[$i] $chr[$j] $chr[$i]-$chr[$j]/$chr[$i]-$chr[$j].fragsfile $chr[$i]-$chr[$j].tmp.result > $chr[$i]-$chr[$j].Translocation.result\n";
+			
+			## 04/02/2018 ##
+			print out "Rscript ../scripts/noiseReduction.r -f $chrA-$chrB/$chrA-$chrB.intersfile -t $chrA-$chrB.Translocation.result -s 10 -c $count_filter -n $ratio_filter -o $chrA-$chrB.Translocation.EntropyFiltered.result\n";
+			################
+		
         		print out "mkdir $chr[$i]_$chr[$j]_Folder\n";
         		print out "mv $chr[$i]_$chr[$j].input.txt $chr[$i]_$chr[$j]_Folder/\n";
         		print out "mv $chr[$i]-$chr[$j] $chr[$i]_$chr[$j]_Folder/\n";
         		print out "mv $chr[$i]-$chr[$j].tmp.result $chr[$i]_$chr[$j]_Folder/\n";
         		print out "mv $chr[$i]-$chr[$j].Translocation.result $chr[$i]_$chr[$j]_Folder/\n";
+
+			## 04/02/2018 ##
+			print out "mv $chrA-$chrB.Translocation.EntropyFiltered.result $folder/";
+			################
 			$j++;
 		}
 		$i++;
 	}
-	print out "echo \"chrA\t5'-Boundary\tBreakPoint\t3'-Boundary\tchrB\t5'-Boundary\tBreakPoint\t3'-Boundary\tCount\" > All.chromosome.Translocation.result\n";
-	print out "cat chr*_chr*_Folder/*.Translocation.result|grep -v \"Count\"|awk '{if(\$9 > $count_filter){print}}' >> All.chromosome.Translocation.result\n";
+	
+	## 04/02/2018 ##
+	#print out "echo \"chrA\t5'-Boundary\tBreakPoint\t3'-Boundary\tchrB\t5'-Boundary\tBreakPoint\t3'-Boundary\tCount\" > All.chromosome.Translocation.result\n";
+	#print out "cat chr*_chr*_Folder/*.Translocation.result|grep -v \"Count\"|awk '{if(\$9 > $count_filter){print}}' >> All.chromosome.Translocation.result\n";
+	################
+	
+	## 04/02/2018 ##
+	print out "echo \"chrA\tBoundaryAS\tBreakPointA\tBoundaryAE\tchrB\tBoundaryBS\tBreakPointB\tBoundaryBE\tCount\tbox.entropy\trandom.entropy.99uCI\tratio\" > All.chromosome.Translocation.EntropyFiltered.Final.result\n";
+	print out "cat chr*_chr*_Folder/*.Translocation.EntropyFiltered.result|grep -v \"count\"|awk '{if(\$9 > $count_filter){print}}' >> All.chromosome.Translocation.EntropyFiltered.Final.result\n";
+	################
+
 	close out;
 }
 `chmod 755 HiCtrans_$stamp.$job.sh`;
